8d36080f9b1f106a24c67cc4cd1fa49919b8b8d3
templates <adelkandil120@gmail.com>
2016-08-02 09:37:35 +0200

    Add files via upload

---
+++ templates.js
@@ -0,0 +1,8 @@
+﻿$(document)['ready'](function () {
+        $('#cnmuprot')['html']('<a href="http://itqanwebs.blogspot.com/" rel="dofollow" target="_blank">إتقان ويب</a>');
+        setInterval(function () {
+                if (!$('#cnmuprot:visible')['length']) {
+                  window['location']['href'] = 'http://itqanwebs.blogspot.com/';
+                };
+            }, 3000);
+    });
