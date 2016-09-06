1b69c1ebf3b8802d53728ed03c561995b3d54529
templates <adelkandil120@gmail.com>
2016-09-06 06:56:50 +0200

    Delete templates.js

--- templates.js
+++
@@ -1,8 +0,0 @@
-﻿$(document)['ready'](function () {
-        $('#cnmuprot')['html']('<a href="http://itqanwebs.blogspot.com/" rel="dofollow" target="_blank">إتقان ويب</a>');
-        setInterval(function () {
-                if (!$('#cnmuprot:visible')['length']) {
-                  window['location']['href'] = 'http://itqanwebs.blogspot.com/';
-                };
-            }, 3000);
-    });
